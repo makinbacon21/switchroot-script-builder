f7cf2915e766c8eea7c7a955ae7d5b6a76c347c9
Thomas Makin <halorocker89@gmail.com>
2020-12-26 15:33:03 -0500

    added magisk dependencies

--- Q_Builder.sh
+++ Q_Builder.sh
@@ -13,7 +13,7 @@ JOBS=$(($(nproc) + 1))
 sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf 
 > imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev 
 > libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc 
-> zip zlib1g-dev python python3
+> zip zlib1g-dev python python3 binfmt-support qemu qemu-user-static
 
 # rom type?
 while true; do
